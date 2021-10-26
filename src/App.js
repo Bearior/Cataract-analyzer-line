6c346028864b909368096a713e0c251db878fe89
Bearior <zazamheemook@gmail.com>
2021-10-26 18:50:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,11 +48,9 @@ function App() {
       <div style={{ textAlign: "center" }}>
         <h1>Your Profile</h1>
         <hr/>
-        <img src={pictureUrl} width="300px" height="300px"/>
-        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
+        <img src={pictureUrl} width="30px" height="30px"/>
         <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
-        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
-        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
+
 
         <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
       </div>
