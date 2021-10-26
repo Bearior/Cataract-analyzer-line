538a4ddd37e10220ce77a9cdf9e839218b5c99a9
Bearior <zazamheemook@gmail.com>
2021-10-26 19:12:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,11 +44,11 @@ function App() {
 
   return (
     <div className="App">
-      {/* <div style={{ textAlign: "right" }}> */}
+       <div style={{ textAlign: "right" }}> 
         <img src={pictureUrl} width="60px" height="60px"/>
         <p style={{ textAlign: "left", marginLeft: "5%", wordBreak: "break-all" }}> {displayName}</p>
-        <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
-      {/* </div> */}
+        <button onClick={() => logout()} style={{ width: 150, height: 30 }}>Logout</button>
+      </div> 
     </div>
   );
 }
