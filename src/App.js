f6d1c044a58353ca3fd6b00e23982f4c6eb29582
Bearior <zazamheemook@gmail.com>
2021-10-26 19:42:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,8 +44,8 @@ function App() {
 
   return (
     <div className="App">
-        <img src={pictureUrl} style={{ marginLeft: "90%" }} width="60px" height="60px"/>
-        <p style={{ marginLeft: "90%", wordBreak: "break-all" }}> {displayName}</p>
+        <img src={pictureUrl} style={{ marginLeft: "100%" }} width="60px" height="60px"/> 
+        <p style={{ marginLeft: "100%", wordBreak: "break-all" }}> {displayName}</p>
         <button onClick={() => logout()} style={{ width: 150, height: 30, marginLeft: "90%" }}>Logout</button>
       </div> 
   );
